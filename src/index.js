ac96802f349f689537e237233ccd2703cd5eade2
kovinant <kabysh_nik@mail.ru>
2024-07-11 15:51:21 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-const characters = [
-  {name: 'мечник', health: 10},
-  {name: 'маг', health: 100},
-  {name: 'маг', health: 0},
-  {name: 'лучник', health: 0},
-];
-    
-
-const alive = characters.filter(item => item.health > 0);
+import './css/style.css';
+import './js/app'
